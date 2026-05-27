b882faaba0908bd1cbfb6486d31add39361a6e12
gtiralongo <gustavotiralongo@gmail.com>
2026-05-27 14:15:06 -0300

    0.3.1 Quitado icon/badge de notificaciones para que Android use icono del manifest

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -41,8 +41,6 @@ messaging.onBackgroundMessage((payload) => {
   const d = payload.data || {};
   self.registration.showNotification(d.title || '¡ZUMBIDO!', {
     body: d.body || (d.fromName ? d.fromName + ' te ha enviado un zumbido' : 'Alguien te ha enviado un zumbido'),
-    icon: 'icon-192.png',
-    badge: 'icon-192.png',
     vibrate: [200, 100, 200],
     requireInteraction: true,
     tag: 'zumbido-' + (d.fromUid || Date.now()),
