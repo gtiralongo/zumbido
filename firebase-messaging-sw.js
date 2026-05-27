cebbf785ce87928b948ed76f8fe797bae57a2495
gtiralongo <gustavotiralongo@gmail.com>
2026-05-27 15:12:22 -0300

    0.4.0 Fix notificaciones: Set IDs en listenBuzzes, error handling, permiso en login, foreground movil, SW clients.claim

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -21,7 +21,7 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
 
